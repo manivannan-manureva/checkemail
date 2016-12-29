8489f8de8bc59e2642a195fe90ee16681a160b0c
manivannan-manureva <manivannan.g@manurevasolutions.com>
2016-12-29 12:26:18 +0530

    12:26 PM

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+// include SMTP Email Validation Class
+require_once('smtp_validateEmail.class.php');
+
+// the email to validate
+$email = 'yogucloud545@gmail.com';
+// an optional sender
+$sender = 'manivannan.g@manurevasolutions.com';
+// instantiate the class
+$SMTP_Validator = new SMTP_validateEmail();
+// turn on debugging if you want to view the SMTP transaction
+$SMTP_Validator->debug = false;
+// do the validation
+$results = $SMTP_Validator->validate(array($email), $sender);
+// view results
+echo $email.' is '.($results[$email] ? 'valid' : 'invalid')."\n";
+
+// send email? 
+if ($results[$email]) {
+  //mail($email, 'Confirm Email', 'Please reply to this email to confirm', 'From:'.$sender."\r\n"); // send email
+    echo 'This is valid email address';
+} else {
+  echo 'The email addresses you entered is not valid';
+}
+?>
